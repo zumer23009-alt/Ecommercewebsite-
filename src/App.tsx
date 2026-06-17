85aebc2b9c456916bd2cf2484801378e4ef81dea
zumer23009-alt <zumer23009@gmail.com>
2026-06-17 07:23:28 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { StoreProvider } from './context/Store';
+import { StoreProvider } from './Storecontext';
 import { useEffect } from 'react';
 
 // Public components
