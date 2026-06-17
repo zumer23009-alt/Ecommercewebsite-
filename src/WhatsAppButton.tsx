d9c594af2718d4084841c8aa0291d7fe8b929afa
zumer23009-alt <zumer23009@gmail.com>
2026-06-17 20:10:50 +0500

    Update WhatsAppButton.tsx

--- src/WhatsAppButton.tsx
+++ src/WhatsAppButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useStore } from './StoreContext';
+import { useStore } from './context/StoreContext';
 
 export default function WhatsAppButton() {
   const [open, setOpen] = useState(false);
