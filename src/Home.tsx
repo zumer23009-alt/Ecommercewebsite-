5b173de98f69625e693c5cf9f2f7156647c6b1de
zumer23009-alt <zumer23009@gmail.com>
2026-06-17 12:56:42 +0500

    Fix Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { store, Product } from '../data/store';
-import ProductCard from '../components/ProductCard';
+import { store, Product } from './store';
+import ProductCard from './ProductCard';
 
 export default function Home() {
   const data = store.load();
