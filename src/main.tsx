138f4dd8e72b4eea29905f34ff64b05b15c11389
zumer23009-alt <zumer23009@gmail.com>
2026-06-19 13:16:29 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { StoreProvider } from "./StoreContext"; // Humne Provider import kiya
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>
-);
